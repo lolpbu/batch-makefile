38aa5bdcea0f386302dea3ed1ce4e316804e03e6
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-08-11 14:25:44 -0300

    ajeitado

--- example_batch.bat
+++ example_batch.bat
@@ -1,9 +1,7 @@
 @ECHO OFF
 SETLOCAL
 
-REM nao funciona pra arquivo src eeem pastas internas 
-
-SET EXE=program.exe
+SET EXE="program.exe"
 SET SRCDIR="."
 SET INCDIR="include"
 SET BINDIR="bin"
@@ -11,11 +9,18 @@ SET EXT="c"
 SET CC="gcc"
 SET OBJS="%BINDIR%/*.o"
 
-IF "%1"=="make" GOTO :MAKE 
 IF "%1"=="create" GOTO :CREATE 
 IF "%1"=="clear" GOTO :CLEAR
+IF "%1"=="help" GOTO :HELP
+GOTO :MAKE 
 
-ECHO "make [make {run}] [createte] [clear]"
+
+
+:HELP
+ECHO "make        -> compile"
+ECHO "make run    -> compile & run"
+ECHO "make create -> create bin, src & include dirs"
+ECHO "make help   -> ..."
 GOTO :EOF
 
 
@@ -26,9 +31,9 @@ REM /m = search mask (* wildcard)
 REM /s = recurse into subfolders
 REM /c = command to each file found 
 FORFILES /P %SRCDIR% /M *.%EXT% /S /C "CMD /C ECHO compilando arquivo @file && %CC% -I %INCDIR% -c @relpath -o %BINDIR%\\@FNAME.o"
-ECHO linkando arquivos em %EXE%
+ECHO "linkando arquivos em %EXE%"
 %CC% %OBJS% -o %EXE%
-ECHO pronto :3
+ECHO "pronto :3"
 
 IF "%2"=="run" (
     CLS
@@ -37,11 +42,18 @@ IF "%2"=="run" (
 
 GOTO :EOF
 
+
+
 :CREATE
-MKDIR %INCDIR% %BINDIR%
+MKDIR %INCDIR% %BINDIR% %SRCDIR%
+GOTO :EOF
+
+
 
 :CLEAR
 DEL /Q /S *.o *.exe %BINDIR%\*
+
+
 
 :EOF
 ENDLOCAL
